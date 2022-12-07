c475ed7d0890ee342c7205b2b14e1e211bd7f525
Evantopian <EvanAcademics@gmail.com>
2022-12-06 19:57:49 -0500

    updated

--- Medium/Med74.cpp
+++ Medium/Med74.cpp
@@ -14,21 +14,18 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
     int r = 0, c = cols - 1;
     while (r < rows && c >= 0)
     {
-        if (matrix[r][matrix[0].size() - 1] > target &&
-            matrix[r][0] < target)
+        if (matrix[r][c] == target)
+            return true;
+        if (matrix[r][matrix[0].size() - 1] > target && matrix[r][0] < target)
         {
-            break;
+            return binary_search(matrix[r].begin(), matrix[r].end(), target);
         }
         if (matrix[r][c] > target)
-        {
             c--;
-        }
         else
-        {
             r++;
-        }
     }
-    return binary_search(matrix[r-1].begin(), matrix[r-1].end(), target);
+    return false;
 }
 
 int main()
